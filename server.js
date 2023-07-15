c01f7b7bdadbd779462bc73693e71e1dc821f389
Anon <anupamkhosla1993@gmail.com>
2023-07-16 02:15:11 +0530

    fixed  baseUrl

--- server.js
+++ server.js
@@ -27,23 +27,17 @@ app.use(express.static(publicDir));
 // Load the /posts routes
 app.use("/posts", posts);
 
-//create a file in "frontend/src/config.js" with the content "export const baseUrl = ${domain}:PORT;"
-//where domain is the domain name of the hosting provider
-//and PORT is the port number of the backend server
-//use fs module to create a file
-//fs.writeFileSync('./frontend/src/config.js',`export const baseUrl = ${domain}:PORT;`);
-
-
-
-
-
+console.log(process.env.NODE_ENV);
 
 
 
 // Global error handling
 app.use((err, _req, res, next) => {
   res.status(500).send("Uh oh! An unexpected error occured.")
-})
+});
+
+
+
 
 // start the Express server
 app.listen(PORT, () => {
--- tests/test.js
+++ tests/test.js
@@ -19,30 +19,14 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-      //test.string(result.text).contains('Musicsheets'); // Musicsheets would be in h1 tag and/or logo
-      
-      //check if hasheader is html
-      //test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
-
-      //convert string to json
-      let url = test.value(result).actual.request.url;
-      //strip port from url and the trailing slash
-      url = url.replace(/:\d+/, '');
-      //strip trailing slash if any
-      url = url.replace(/\/$/, '');
-      console.log('url: ' + url);
-
-
-      //print current directory
-      console.log('current dir: ' + process.cwd());
-
-      fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = '${url}:${PORT}';`);
-
-      console.log(url);
-      //throw error
-      throw new Error('error');
-
-
+      test.string(result.text).contains('Musicsheets');
+      test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
+      if(!!process.env.NODE_ENV) {
+        fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'musicsheets.in:${PORT}';`);
+      }
+      else {
+        fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'localhost:${PORT}';`);
+      }
       done(err);
     });
   });
