8bd4e1b56865c338b4c424475fefc76ae3068d83
Anon <anupamkhosla1993@gmail.com>
2023-07-28 22:35:59 +0530

    fixing baseUrl with new build stage

--- setBaseUrl.js
+++ setBaseUrl.js
@@ -10,7 +10,7 @@ console.log("Wrote baseUrl to frontend/src/config.js");
 //use try catch
 try {  
   if(!!process.env.PARTITION) { //you might use NODE_ENV
-    fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'http://16.171.73.224';`);    
+    fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = '16.171.73.224';`);    
     console.log("Wrote http://16.171.73.224 to config.js");
   }
   else {
