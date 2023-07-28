7621c8e95b632891777f9a5311bf33108c20e705
Anon <anupamkhosla1993@gmail.com>
2023-07-28 14:20:10 +0530

    changing url to ip

--- setBaseUrl.js
+++ setBaseUrl.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5050;
 
 console.log("Wrote baseUrl to frontend/src/config.js");
 if(!!process.env.PARTITION) { //you might use NODE_ENV
-  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'musicsheets.in';`);      
+  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'http://16.171.73.224';`);      
 }
 else {
  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'localhost:${PORT}';`);
