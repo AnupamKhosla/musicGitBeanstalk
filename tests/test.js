75a6658962468a14f54901ea5e14514b20d5a79e
Anon <anupamkhosla1993@gmail.com>
2023-07-28 13:53:18 +0530

    removed ebextentions old configs

--- tests/test.js
+++ tests/test.js
@@ -1,24 +1,24 @@
 'use strict';
 
-// const supertest = require('supertest'); 
-// const test = require('unit.js');
-// const app = require('../server.js');
+const supertest = require('supertest'); 
+const test = require('unit.js');
+const app = require('../server.js');
 
 
-//convert requires to imports
-// import supertest from 'supertest';
-// import test from 'unit.js';
-// import app from '../server.js';
+convert requires to imports
+import supertest from 'supertest';
+import test from 'unit.js';
+import app from '../server.js';
 
 
-// const request = supertest(app);
+const request = supertest(app);
 
-// describe('Tests app', function() {
-//   it('verifies get', function(done) {
-//     request.get('/').expect(200).end(function(err, result) {
-//       //test.string(result.text).contains('Musicsheets');
-//       //test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');      
-//       done(err);
-//     });
-//   });
-// });
+describe('Tests app', function() {
+  it('verifies get', function(done) {
+    request.get('/').expect(200).end(function(err, result) {
+      //test.string(result.text).contains('Musicsheets');
+      //test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');      
+      done(err);
+    });
+  });
+});
