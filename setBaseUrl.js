1b5ba0bb332ab0b05123a40576ca163d05bbab51
Anon <anupamkhosla1993@gmail.com>
2023-07-28 15:15:31 +0530

    debuggin setBaseUrl

--- setBaseUrl.js
+++ setBaseUrl.js
@@ -6,8 +6,10 @@ const PORT = process.env.PORT || 5050;
 
 console.log("Wrote baseUrl to frontend/src/config.js");
 if(!!process.env.PARTITION) { //you might use NODE_ENV
-  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'http://16.171.73.224';`);      
+  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'http://16.171.73.224';`);    
+  console.log("Wrote http://16.171.73.224 to config.js");
 }
 else {
  fs.writeFileSync('./frontend/src/config.js', `export const baseUrl = 'localhost:${PORT}';`);
+console.log(`Wrote localhost:${PORT} to config.js`);
 }
