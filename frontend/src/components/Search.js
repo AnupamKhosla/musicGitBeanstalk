25fe34963532c723d36364f88b7ab97fa2e1d735
Anon <anupamkhosla1993@gmail.com>
2023-08-19 16:17:04 +1000

    css search updates

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -22,52 +22,43 @@ export default function Search(props) {
 	                <div className="p-6 bg-white dark:bg-slate-900 rounded-md shadow-md dark:shadow-gray-800">
 	                    <form action="#">
 	                        <div className="registration-form relative text-dark text-start">
-	                            <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-0 gap-6">
-	                                <div className="filter-search-form relative lg:before:content-[''] lg:before:absolute lg:before:top-[10px] lg:before:end-0 lg:before:h-10 lg:before:z-1 lg:before:border-r lg:before:border-inherit lg:before:rounded-md lg:before:outline-0 lg:dark:before:border-gray-700">
-	                                    <i className="uil uil-search absolute top-[48%] -translate-y-1/2 start-3 z-1 text-rose-600 text-[20px]"></i>
-	                                    <input name="name" type="text" id="job-keyword" className="form-input lg:rounded-t-sm lg:rounded-e-none lg:rounded-b-none lg:rounded-s-sm lg:outline-0 w-full filter-input-box bg-gray-50 dark:bg-slate-800 border-0 focus:ring-0" placeholder="Search your keaywords" />
+	                            <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:gap-0 gap-6">	                            	
+	                                <div className="filter-search-form relative ">
+	                                    <label htmlFor="songName" className="font-semibold text-sm mb-2">Song name</label>
+	                                    <i className="uil uil-search absolute -bottom-[9px] -translate-y-1/2 start-3 z-1 text-rose-600 text-[20px]"></i>
+	                                    <input 
+	                                      name="songName"
+	                                      id="songName"
+	                                      type="text"
+	                                      className="pl-10 form-input lg:rounded-t-sm lg:rounded-e-none lg:rounded-b-none lg:rounded-s-sm lg:outline-0 w-full filter-input-box bg-gray-100 dark:bg-slate-800 border-0 focus:ring-0"
+	                                      placeholder="E.g. Sonata" />
 	                                </div>
-
-	                                <div className="filter-search-form relative lg:before:content-[''] lg:before:absolute lg:before:top-[10px] lg:before:end-0 lg:before:h-10 lg:before:z-1 lg:before:border-r lg:before:border-inherit lg:before:rounded-md lg:before:outline-0 lg:dark:before:border-gray-700">
-	                                    <i className="uil uil-estate absolute top-[48%] -translate-y-1/2 start-3 z-1 text-rose-600 text-[20px]"></i>
-	                                    <select className="form-select z-2" data-trigger name="choices-catagory" id="choices-catagory" aria-label="Default select example">
-	                                        <option>Houses</option>
-	                                        <option>Apartment</option>
-	                                        <option>Offices</option>
-	                                        <option>Townhome</option>
-	                                    </select>
+	                                <div className="ml-2 filter-search-form relative ">
+	                                    <label htmlFor="artistName" className="font-semibold text-sm mb-2">Artist name</label>
+	                                    <input 
+	                                      name="songName"
+	                                      id="songName"
+	                                      type="text"
+	                                      className="form-input lg:rounded-t-sm lg:rounded-e-none lg:rounded-b-none lg:rounded-s-sm lg:outline-0 w-full filter-input-box bg-gray-100 dark:bg-slate-800 border-0 focus:ring-0"
+	                                      placeholder="E.g. Mozart" />
 	                                </div>
-	                            
-	                                <div className="filter-search-form relative lg:before:content-[''] lg:before:absolute lg:before:top-[10px] lg:before:end-0 lg:before:h-10 lg:before:z-1 lg:before:border-r lg:before:border-inherit lg:before:rounded-md lg:before:outline-0 lg:dark:before:border-gray-700">
-	                                    <i className="uil uil-usd-circle absolute top-[48%] -translate-y-1/2 start-3 z-1 text-rose-600 text-[20px]"></i>
-	                                    <select className="form-select" data-trigger name="choices-min-price" id="choices-min-price" aria-label="Default select example">
-	                                        <option>Min Price</option>
-	                                        <option>500</option>
-	                                        <option>1000</option>
-	                                        <option>2000</option>
-	                                        <option>3000</option>
-	                                        <option>4000</option>
-	                                        <option>5000</option>
-	                                        <option>6000</option>
-	                                    </select>
+	                                <div className="ml-2 filter-search-form relative ">
+	                                    <label htmlFor="artistName" className="font-semibold text-sm mb-2">Scale/Raga</label>
+	                                    <input 
+	                                      name="scaleName"
+	                                      id="scaleName"
+	                                      type="text"
+	                                      className="form-input lg:rounded-t-sm lg:rounded-e-none lg:rounded-b-none lg:rounded-s-sm lg:outline-0 w-full filter-input-box bg-gray-100 dark:bg-slate-800 border-0 focus:ring-0"
+	                                      placeholder="E.g. Major" />
 	                                </div>
-	                            
-	                                <div className="filter-search-form relative">
-	                                    <i className="uil uil-usd-circle absolute top-[48%] -translate-y-1/2 start-3 z-1 text-rose-600 text-[20px]"></i>
-	                                    <select className="form-select" data-trigger name="choices-max-price" id="choices-max-price" aria-label="Default select example">
-	                                        <option>Max Price</option>
-	                                        <option>500</option>
-	                                        <option>1000</option>
-	                                        <option>2000</option>
-	                                        <option>3000</option>
-	                                        <option>4000</option>
-	                                        <option>5000</option>
-	                                        <option>6000</option>
-	                                    </select>
-	                                </div>
-
-	                                <div className="lg:mt-6">
-	                                    <input type="submit" id="search" name="search" className="py-2 px-5 inline-block font-semibold tracking-wide border align-middle transition duration-500 ease-in-out text-base text-center bg-rose-600 hover:bg-rose-700 border-rose-600 hover:border-rose-700 text-white searchbtn w-full !h-12 rounded" value="Search" />
+	                                <div className="lg:mt-6 flex items-end">
+	                                    <input 
+	                                      type="submit" 
+	                                      id="search" 
+	                                      name="search" 
+	                                      className="ml-2 py-2 px-5 cursor-pointer inline-block font-semibold tracking-wide border align-middle transition duration-500 ease-in-out text-base text-center bg-rose-600 hover:bg-rose-700 border-rose-600 hover:border-rose-700 text-white searchbtn w-full !h-10 rounded"
+	                                      value="Search"
+	                                    />
 	                                </div>
 	                            </div>
 	                        </div>
